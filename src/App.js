b99ab5b114c81e30efa4f696caa4f95e069c84b7
Dejah Cooper <Dejah.C@icloud.com>
2025-02-26 14:46:15 -0500

    added test comps to file

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import Pokecard from './Pokecard'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      
+      <Pokecard name="Bulbasaur" id={1} type="Grass" />
+      <Pokecard name="Charmander" id={4} type="Fire" />
+      <Pokecard name="Squirtle" id={7} type="Water" />
+
+    </>
   );
 }
 
