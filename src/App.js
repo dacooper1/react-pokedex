74b334bf1faa714d55a800347a07884f17f709a9
Dejah Cooper <Dejah.C@icloud.com>
2025-02-26 15:03:25 -0500

    added Pokedex comp

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import Pokecard from './Pokecard';
+import Pokedex from './Pokedex';
 
 function App() {
   return (
     <>
-      
-      <Pokecard name="Bulbasaur" id={1} type="Grass" />
-      <Pokecard name="Charmander" id={4} type="Fire" />
-      <Pokecard name="Squirtle" id={7} type="Water" />
-
+      <Pokedex />
     </>
   );
 }
