d47cdac10e33b8333e0a596bea3f335decca21d3
Dejah Cooper <Dejah.C@icloud.com>
2025-02-26 15:24:56 -0500

    fixed default props bug

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import Pokecard from './Pokecard';
 
-const Pokedex = (props) => {
+const Pokedex = ({pokemon = [
+  { id: 4, name: "Charmander", type: "fire", base_experience: 62 },
+  { id: 7, name: "Squirtle", type: "water", base_experience: 63 },
+  { id: 11, name: "Metapod", type: "bug", base_experience: 72 },
+  { id: 12, name: "Butterfree", type: "flying", base_experience: 178 },
+  { id: 25, name: "Pikachu", type: "electric", base_experience: 112 },
+  { id: 39, name: "Jigglypuff", type: "normal", base_experience: 95 },
+  { id: 94, name: "Gengar", type: "poison", base_experience: 225 },
+  { id: 133, name: "Eevee", type: "normal", base_experience: 65 },
+  ]
+}) => {
   return (
     <div>
       <h1>Pokedex</h1>
       <div className="pokedex">
-        {props.pokemon.map((p) => (
+        {pokemon.map((p) => (
           <Pokecard key={p.id} id={p.id} name={p.name} type={p.type} exp={p.base_experience} />
         ))}
       </div>
@@ -14,18 +24,5 @@ const Pokedex = (props) => {
   );
 };
 
-
-Pokedex.defaultProps = {
-  pokemon: [
-    { id: 4, name: "Charmander", type: "fire", base_experience: 62 },
-    { id: 7, name: "Squirtle", type: "water", base_experience: 63 },
-    { id: 11, name: "Metapod", type: "bug", base_experience: 72 },
-    { id: 12, name: "Butterfree", type: "flying", base_experience: 178 },
-    { id: 25, name: "Pikachu", type: "electric", base_experience: 112 },
-    { id: 39, name: "Jigglypuff", type: "normal", base_experience: 95 },
-    { id: 94, name: "Gengar", type: "poison", base_experience: 225 },
-    { id: 133, name: "Eevee", type: "normal", base_experience: 65 },
-  ],
-};
 
 export default Pokedex;
