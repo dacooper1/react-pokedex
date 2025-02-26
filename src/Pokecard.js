80cbedb395fd798b35a4347601bd3ff249c45d8a
Dejah Cooper <Dejah.C@icloud.com>
2025-02-26 15:29:25 -0500

    added classNames, created css and imported css file

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import './Pokecard.css';
 
 const Pokecard = (props) => {
     const imgSrc = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${props.id}.png`
 
     return (
         <>
-            <div> Name: {props.name} </div>
-            <img src={imgSrc} alt={`${props.name}'s image`}></img>
-            <div> Type: {props.type} </div>
+            <div className="Pokecard-name"> Name: {props.name} </div>
+            <img className="Pokecard-img" src={imgSrc} alt={`${props.name}'s image`}></img>
+            <div className="Pokecard-type"> Type: {props.type} </div>
         </>
     )
 }
