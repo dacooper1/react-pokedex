02e4e4a8bce27d5d79b901c0c885b82c6fa69e09
Dejah Cooper <Dejah.C@icloud.com>
2025-03-04 17:22:44 -0500

    changed react ragment to div to add className

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -5,12 +5,14 @@ const Pokecard = (props) => {
     const imgSrc = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${props.id}.png`
 
     return (
-        <>
+        <div className="Pokecard">
             <div className="Pokecard-name">{props.name} </div>
             <img className="Pokecard-img" src={imgSrc} alt={`${props.name}'s image`}></img>
             <div className="Pokecard-type"> Type: {props.type} </div>
-        </>
+        </div>
     )
 }
+
+
 
 export default Pokecard;
